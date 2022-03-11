ffd87e2644ba50381697e927d33d6783519583e7
chenrui <chenrui@jingdaka.com>
2022-03-11 14:29:59 +0800

    chore: remove comments

--- scopes/update.go
+++ scopes/update.go
@@ -9,16 +9,6 @@ type Config struct {
 
 func UpdateScope(c *Config) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		// org := r.Query("org")
-
-		// if org != "" {
-		// 	var organization Organization
-		// 	if db.Session(&Session{}).First(&organization, "name = ?", org).Error == nil {
-		// 		return db.Where("org_id = ?", organization.ID)
-		// 	}
-		// }
-
-		// db.AddError("invalid organization")
 		return db
 	}
 }
